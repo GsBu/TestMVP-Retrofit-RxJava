792e88e96af18f334c6d57c01339c5d768055b7f
longlong <longlong.bu@camdora.me>
2017-11-21 15:30:00 +0800

    增加RxJava

--- TestMVP-Retrofit-RxJava/app/src/main/java/com/example/camdora/testmvp_retrofit_rxjava/MainActivity.java
+++ TestMVP-Retrofit-RxJava/app/src/main/java/com/example/camdora/testmvp_retrofit_rxjava/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.camdora.testmvp_retrofit_rxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.example.camdora.testmvp_retrofit_rxjava.model.Book;
@@ -11,9 +12,15 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     private TextView text;
 
@@ -25,20 +32,33 @@ public class MainActivity extends AppCompatActivity {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.douban.com/v2/")
                 .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().create()))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())//支持RxJava
                 .build();
 
         text = (TextView)findViewById(R.id.tv_1);
 
         RetrofitService service = retrofit.create(RetrofitService.class);
-        Call<Book> call =  service.getSearchBook("金瓶梅", null, 0, 1);
-        call.enqueue(new Callback<Book>() {
-            @Override
-            public void onResponse(Call<Book> call, Response<Book> response) {
-                text.setText(response.body()+"");
-            }
-            @Override
-            public void onFailure(Call<Book> call, Throwable t) {
-            }
-        });
+        Observable<Book> observable =  service.getSearchBook("金瓶梅", null, 0, 1);
+
+        observable.subscribeOn(Schedulers.io())//请求数据的事件发生在io线程
+                .observeOn(AndroidSchedulers.mainThread())//请求完成后在主线程更新UI
+                .subscribe(new Observer<Book>() {//订阅
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, e.getMessage());
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onNext(Book book) {
+                        text.setText(book.toString());
+                    }
+                });
+
     }
 }
--- TestMVP-Retrofit-RxJava/app/src/main/java/com/example/camdora/testmvp_retrofit_rxjava/RetrofitService.java
+++ TestMVP-Retrofit-RxJava/app/src/main/java/com/example/camdora/testmvp_retrofit_rxjava/RetrofitService.java
@@ -5,6 +5,7 @@ import com.example.camdora.testmvp_retrofit_rxjava.model.Book;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by camdora on 17-11-21.
@@ -12,8 +13,8 @@ import retrofit2.http.Query;
 
 public interface RetrofitService {
     @GET("book/search")
-    Call<Book> getSearchBook(@Query("q") String name,
-                             @Query("tag") String tag,
-                             @Query("start") int start,
-                             @Query("count") int count);
+    Observable<Book> getSearchBook(@Query("q") String name,
+                                   @Query("tag") String tag,
+                                   @Query("start") int start,
+                                   @Query("count") int count);
 }
